ed96d806a909bf3c957400beff5ca4eded602593
Chris Goldsworthy <christopher.goldsworthy@outlook.com>
2019-04-26 15:06:07 -0400

    run_ingot_memcached.sh: replace hard-coded vals with vars
    Make the script more general, to keep off technical debt. Include options for
    running memcached, for running memcached through gdb, or for running the
    debug-instrumented memcached (memcached-debug).

--- run_ingot_memcached.sh
+++ run_ingot_memcached.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+HOST="127.0.0.1"
+PORT="12345"
+MEMCACHED_OPTS=" -l $HOST -p $PORT -m 64 -vv"
+
 # Start off by cleaning up ingot related items
 echo "Cleaning up Atlas stuff"
-rm -rf /dev/shm/`whoami`/*
+rm -r /dev/shm/`whoami`/*
 rm persist.log
+echo "yes" | ../Atlas/runtime/build/tools/clean_mem
 
-echo "Running memcached on 127.0.0.1:12345"
-./memcached -l 127.0.0.1 -p 12345 -m 64 -vv
+if [ -z "$1" ]; then
+    CMD="./memcached $MEMCACHED_OPTS"
+elif [ "$1" == "d" ]; then
+    echo "Removing ./core"
+    rm -f core
+    CMD="./memcached-debug $MEMCACHED_OPTS"
+elif [ "$1" == "g" ]; then
+    CMD="gdb --args ./memcached-debug $MEMCACHED_OPTS"
+else
+    echo "Not a valid choice."
+    exit 1
+fi
+
+# Run
+echo "Running $CMD"
+exec $CMD
 
